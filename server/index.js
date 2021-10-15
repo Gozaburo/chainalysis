1c47cd9756527a1cb2ca5e6da4e50e7f2a42aea9
K <freebiesforforgg@gmail.com>
2021-10-15 18:31:55 -0500

    A

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(express.static(path.resolve(__dirname, '..client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.get("/api", (req, res) => {
     console.log('Got api request');
     res.json({
